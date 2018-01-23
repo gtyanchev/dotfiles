c12294f593fcb8fa94b64a420d63bd5cfc0640f1
Georgi Georgiev <gtqnchev@gmail.com>
2018-01-23 08:51:27 +0200

    Add rbenv shims to emacs env

--- .emacs.d/setup/init-ruby.el
+++ .emacs.d/setup/init-ruby.el
@@ -29,4 +29,8 @@
   :config
   (rspec-install-snippets))
 
+;; Setting rbenv path
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
+
 (provide 'init-ruby)
