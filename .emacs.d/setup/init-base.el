8fe2bb6e5fe73698a6161475084285bd63c5bb6d
Georgi Georgiev <georgi.georgiev@fundingcircle.com>
2020-06-06 14:15:46 +0300

    Setup dumb-jump

--- .emacs.d/setup/init-base.el
+++ .emacs.d/setup/init-base.el
@@ -254,6 +254,16 @@
   :config
   (setq ag-highlight-search t))
 
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g k" . dumb-jump-back)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+  :ensure)
+
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/bin" exec-path))
 
